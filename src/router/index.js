db1a29fa0708e7d3089282c98a08b4318c972816
thevueguy <jose@thevueguy.com>
2020-05-12 08:37:16 -0400

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Services from '../views/Services.vue'
+import Contact from '../views/Contact.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/services',
+    name: 'services',
+    component: Services
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
